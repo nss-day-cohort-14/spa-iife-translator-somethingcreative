189a065a6bfe7fe0e92355279c3f45521b1179d3
Jack Mocherman <mochermanjack@gmail.com>
2016-06-08 15:34:02 -0500

    spanish augmentor main functions

--- spanish.js
+++ spanish.js
@@ -1,9 +1,16 @@
 var Card = (function (spanishCard){
 
-  var spanish = {"merry":"feliz", "christmas":"navidad", "and":"y", "happy":"próspero", "new":"año", "year":"nuevo"}
+  var spanish = {"merry":"feliz", "christmas":"navidad", "and":"y", "happy":"próspero", "new":"año", "year":"nuevo"};
 
+  spanishCard.getSpanish = function () {
+    return spanish;
+  };
 
+  spanishCard.translateToSpanish = function (userText) {
+    text = spanish[userText]
+    Card.translate(text)
+  }
 
-  return spanishCard
+  return spanishCard;
 
-})();
+})(Card);
