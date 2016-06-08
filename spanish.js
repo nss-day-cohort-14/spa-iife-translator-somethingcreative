193593d62b936b406a084269ac9b18f9ff98bb3d
Jack Mocherman <mochermanjack@gmail.com>
2016-06-08 14:31:26 -0500

    built spanish lexicon

---
+++ spanish.js
@@ -0,0 +1,9 @@
+var Card = (function (spanishCard){
+
+  var spanish = {"merry":"feliz", "christmas":"navidad", "and":"y", "happy":"próspero", "new":"año", "year":"nuevo"}
+
+
+
+  return spanishCard
+
+})();
