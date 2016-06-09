e6b6e4b2e3a2174836cf43a118fb35aca912216b
Jack Mocherman <mochermanjack@gmail.com>
2016-06-09 09:14:23 -0500

    DOM Handler javascript file

---
+++ DOMhandler.js
@@ -0,0 +1,27 @@
+var button = document.getElementById('translateButton').addEventListener("click", buildString);
+var input = document.getElementById('inputText');
+var select = document.getElementById('select');
+var stringArray;
+var string;
+
+function buildString () {
+  string = input.value;
+  splitString (string);
+}
+
+function splitString (string) {
+  stringArray = string.split(' ')
+  console.log("", stringArray);
+  if(select.value == "Spanish")
+    {
+        Card.translateToSpanish(stringArray);
+    }
+        if(select.value == "Portuguese")
+    {
+        Card.translateToPortuguese(stringArray);
+    }
+        if(select.value == "Italian")
+    {
+        Card.translateToItalian(stringArray);
+    }
+}
